f4c93e07d8e478ce161e50850cb9cd9059a20adc
asifalam515 <asibulalam515@gmail.com>
2024-12-17 15:36:39 +0600

    latest

--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -1,9 +1,10 @@
 import { Blog } from "@/types";
 import LatestBlogCard from "../ui/LatestBlogCard";
+import BlogCard from "../ui/BlogCard";
 
 const LatestBlogs = ({ blogs }: { blogs: Blog[] }) => {
   return (
-    <div>
+    <div className="w-[90%] mx-auto">
       <h1 className="text-4xl text-center m">
         Latest Blogs from <span className="text-accent">Blogiz</span>{" "}
       </h1>
@@ -14,9 +15,14 @@ const LatestBlogs = ({ blogs }: { blogs: Blog[] }) => {
           dicta!
         </i>{" "}
       </p>
-      <div className="grid grid-cols-2 gap-4">
-        {blogs.map((blog) => (
+      <div className="grid grid-cols-2 gap-4 mt-5 my-5">
+        {blogs.slice(0, 2).map((blog) => (
           <LatestBlogCard key={blog.id} blog={blog}></LatestBlogCard>
+        ))}
+      </div>
+      <div className="grid grid-cols-3 gap-4 mt-5 my-5">
+        {blogs.slice(3, 6).map((blog) => (
+          <BlogCard key={blog.id} blog={blog}></BlogCard>
         ))}
       </div>
     </div>
